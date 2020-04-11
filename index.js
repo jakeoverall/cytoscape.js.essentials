8a7cd7bea34d02ef70ced5791e36bd80afdce8c4
Jake Overall <joverall22@gmail.com>
2020-04-11 06:02:44 -0600

    built files

---
+++ index.js
@@ -0,0 +1,9 @@
+import cytoscape from "./dist/cytoscape.cjs"
+import coseBilkent from "./dist/cytoscape-cose-bilkent"
+import ctxmenu from "./dist/cytoscape-cxtmenu"
+
+export default {
+  cytoscape,
+  coseBilkent,
+  ctxmenu
+}
